a62d15017913f3c7290ded97eace8f957bf0723d
lamasurfer <7376771@gmail.com>
2021-01-14 10:34:25 +0300

    copied classes for task2

---
+++ src/main/java/org/example/task2/Main.java
@@ -0,0 +1,24 @@
+package org.example.task2;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final List<String> VALID_PATHS = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html",
+                "/styles.css", "/app.js", "/classic.html", "/links.html", "/forms.html", "/events.html", "/events.js");
+
+        final int MAX_CONNECTIONS = 64;
+        final int PORT = 9999;
+
+        final Server server = new Server();
+
+        server.setExecutorService(Executors.newFixedThreadPool(MAX_CONNECTIONS));
+        server.setValidPaths(VALID_PATHS);
+
+        server.listen(PORT);
+
+    }
+}
